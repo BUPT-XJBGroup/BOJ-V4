c16a4429b0535e320d6548ea6033454891c5cc6f
apple <apple@appledeMacBook-Pro.local>
2019-03-09 16:28:53 +0800

    add commit
    ValueError at /accounts/groups/add
    Redis URL must specify one of the followingschemes (redis://, rediss://, unix://)
    
    主页没有公告表

--- manage.py
+++ manage.py
@@ -8,5 +8,10 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bojv4.settings")
 
     from django.core.management import execute_from_command_line
+    import sys
+
+    reload(sys)
+
+    sys.setdefaultencoding('utf-8')
 
     execute_from_command_line(sys.argv)
